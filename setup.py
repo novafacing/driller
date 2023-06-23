108d48c7dd21fb53de589cee125df69234cec278
novafacing <rowan.hart@intel.com>
2023-06-23 11:54:29 -0700

    Use old angr

--- setup.py
+++ setup.py
@@ -1,29 +1,32 @@
 from distutils.core import setup
 import subprocess
 
-#TRACER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer'
-#FUZZER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/fuzzer.git#egg=fuzzer'
+# TRACER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/tracer.git#egg=tracer'
+# FUZZER_URL = 'git+ssh://git@git.seclab.cs.ucsb.edu:/cgc/fuzzer.git#egg=fuzzer'
 #
-#if subprocess.call(['pip', 'install', TRACER_URL]) != 0:
+# if subprocess.call(['pip', 'install', TRACER_URL]) != 0:
 #   raise LibError("Unable to install tracer")
 #
-#if subprocess.call(['pip', 'install', FUZZER_URL]) != 0:
+# if subprocess.call(['pip', 'install', FUZZER_URL]) != 0:
 #   raise LibError("Unable to install fuzzer")
 
 setup(
-        name='driller',
-        version='1.0',
-        packages=['driller'],
-        data_files=[
-            ('bin/driller', ('bin/driller/listen.py',),),
-        ],
-        install_requires=[
-            'cle',
-            'angr',
-            'redis',
-            'celery',
-            'archinfo',
-            'dpkt',
-            'termcolor',
-        ],
+    name="driller",
+    version="1.0",
+    packages=["driller"],
+    data_files=[
+        (
+            "bin/driller",
+            ("bin/driller/listen.py",),
+        ),
+    ],
+    install_requires=[
+        "cle==8.20.7.27",
+        "angr==8.20.7.27",
+        "redis",
+        "celery",
+        "archinfo==8.20.7.27",
+        "dpkt",
+        "termcolor",
+    ],
 )
